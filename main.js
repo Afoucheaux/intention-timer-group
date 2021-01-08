a27435a7deaa877797eb1d20ad81ffdd6cb11837
Angela <angela.battillo@gmail.com>
2021-01-07 17:02:09 -0700

    Build out color button hide with unhide functionality

--- main.js
+++ main.js
@@ -3,21 +3,42 @@ var studyButton = document.querySelector('#study');
 var meditateButton = document.querySelector('#meditate');
 var exerciseButton = document.querySelector('#exercise');
 var form = document.querySelector('#form');
+var litStudyButton = document.querySelector('#lit-study');
+var litMeditateButton = document.querySelector('#lit-meditate');
+var litExerciseButton = document.querySelector('#lit-exercise');
 //Event Listeners
 form.addEventListener('click', function(event){
   changeButtonColor();
 });
-
-//Functions:
+//Functions
+function removeCategoryColor(){
+  hide(litStudyButton);
+  hide(litExerciseButton);
+  hide(litMeditateButton);
+  unhide(studyButton);
+  unhide(exerciseButton);
+  unhide(meditateButton);
+}
 function changeButtonColor() {
+  removeCategoryColor();
  if (event.target.id === 'study'){
-  //light study button
+  hide(studyButton);
+  unhide(litStudyButton);
 } else if (event.target.id === 'meditate') {
-  //light meditate button
+  hide(meditateButton);
+  unhide(litMeditateButton);
  } else if (event.target.id === 'exercise') {
-   //light exercise button
+   hide(exerciseButton);
+   unhide(litExerciseButton);
  }
 };
+function hide(element) {
+  element.classList.add('hidden');
+}
+function unhide(element) {
+  element.classList.remove('hidden');
+}
+
 //parent.addEventListener('click, function(event)'){
 //if (event.target.className === 'buttonName') {
 //run the change button function here
